f2a9f05f789191a536d9f1de1524b5effdb0b1ab
Kuldeep Sahrma <kldpsh77@gmail.com>
2023-09-17 17:49:50 +0530

    Duplicate user prevented

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,13 +5,20 @@ module.exports.getSignUp = (req,res,next)=>{
     res.sendFile(path.join(__dirname,'../','views','signup.html'))
 };
 module.exports.postSignUp = (req,res,next)=>{
-    User.create({
-        email:req.body.Email,
-        name:req.body.Name,
-        password:req.body.Password
-    })
-    .then(()=>{
-        res.send('Success')
-    })
-    .catch(err=>console.log(err))
+    let userAlreadyeExist=null;
+    User.findByPk(req.body.Email).then(user=>{userAlreadyeExist=user}).catch(e=>console.log(e))
+    if(userAlreadyeExist==null){
+        res.send('User with this email already exists')
+    }
+    else{
+        User.create({
+            email:req.body.Email,
+            name:req.body.Name,
+            password:req.body.Password
+        })
+        .then(()=>{
+            res.send('Success')
+        })
+        .catch(err=>console.log(err))
+    }
 }
